74983e237d8db2c66d62f7faafcc95e42ecc3218
Abhijit Bhattacharya <abhijit81kol@gmail.com>
2020-09-25 23:38:01 +0530

    Logout implementation

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,7 +8,10 @@ const navigationItems = (props) => (
     <NavigationItem link="/" exact>Burger Builder</NavigationItem>
     {/* <NavigationItem link="/">Checkout</NavigationItem> */}
     <NavigationItem link="/orders">Orders</NavigationItem>
-    <NavigationItem link="/auth">Authenticate</NavigationItem>
+    { !props.isAuthenticated 
+        ?<NavigationItem link="/auth">Authenticate</NavigationItem>
+        :<NavigationItem link="/logout">Logout</NavigationItem>
+    } 
   </ul>
 );
 
